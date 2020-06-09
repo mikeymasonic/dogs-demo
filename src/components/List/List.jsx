8f2c643baa93e7c1be46eddf79d7d4a4cef77c53
Michael Romay <amerror@gmail.com>
2020-06-09 12:51:15 -0700

    created List Component

---
+++ src/components/List/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const List = ({ breed }) => (
+  <section>
+    <p><Link to={`detail/${breed}`} >{breed}</Link></p>
+  </section>
+);
+
+List.propTypes = {
+  breed: PropTypes.string.isRequired,
+};
+
+export default List;
