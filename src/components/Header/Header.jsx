8f52935996c334156a0c7207d9957cad3c3b1343
Michael Romay <amerror@gmail.com>
2020-06-11 13:06:49 -0700

    made Dogs header a link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSearchText, useHandleChange } from '../../hooks/breedProvider';
 import styles from './Header.css';
 
@@ -9,13 +9,10 @@ const Header = () => {
   const handleChange = useHandleChange();
   
   return (
-    <>
-      <section className={styles.Header}>
-        <h1>Dogs!</h1>
-        {/* <Link to="/" >Home</Link> */}
-        <input type="text" placeholder="Search" value={searchText} onChange={handleChange} />
-      </section>
-    </>
+    <section className={styles.Header}>
+      <Link to="/" ><h1>Dogs!</h1></Link>
+      <input type="text" placeholder="Search" value={searchText} onChange={handleChange} />
+    </section>
   );
 };
 
